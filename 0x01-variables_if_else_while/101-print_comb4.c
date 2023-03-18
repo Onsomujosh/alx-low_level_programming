88a5c223abf73014b88132cce2813aec360035c4
Onsomujosh <Joshuaonsomu@gmail.com>
2023-03-17 19:31:32 -0700

    combination of three

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = '0'; a < '9'; a++)
+	for (a = 48; a < 58; a++)
 	{
-		for (b = a + 1; b < '9'; b++)
+		for (b = 49; b < 58; b++)
 		{
-			for (c = b + 1; c < '9'; c++)
+			for (c = 50; c < 58; c++)
 			{
-				if ((b != a) !=c)
+				if (c > b && b > a)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					if (a == '7' && b == '8')
+					if (a != 55 || b != 56)
 					{
 					putchar(',');
 					putchar(' ');
