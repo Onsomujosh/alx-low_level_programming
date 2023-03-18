a873fb5a197fb8d4a2c9107bd00f4c32d7d93cd8
Onsomujosh <Joshuaonsomu@gmail.com>
2023-03-17 18:54:03 -0700

    combination of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 48; a < 55; a++)
+	for (a = 48; a < 56; a++)
 	{
 		for (b = 49; b < 57; b++)
 		{
-			for (c = 50; c < 59; c++)
+			for (c = 50; c < 58; c++)
 			{
 				if (c > b && b > a)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					if (a != 55 || b != 57)
+					if (a != 56 || b != 57)
 					{
 					putchar(',');
 					putchar(' ');
