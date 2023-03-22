8829bf5022894e79141b4553cf2a901165679f7d
Onsomujosh <Joshuaonsomu@gmail.com>
2023-03-22 16:58:22 -0700

    green light

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-include "main.h"
+#include "main.h"
 
 /**
  * print_alphabet - prints lowercase alphabet followed by new line
  *
  * Return: Always 0(Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
