8e3209bd630d5e6570ea531aeca11fab8a3e4fc5
Onsomujosh <Joshuaonsomu@gmail.com>
2023-03-28 00:37:03 -0700

    half the string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * followed by a new line
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int leng = 0;
+	int i = 0;
+	char *s = str;
+	int o;
+
+	while (*s != '\0')
+	{
+		s++;
+		leng++;
+	}
+	i = leng - 1;
+	for (o = 0; o <= i; i++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
