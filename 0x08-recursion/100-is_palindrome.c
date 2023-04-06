757d0b32989caa0617794a23abc74148f39b888d
Onsomujosh <Joshuaonsomu@gmail.com>
2023-04-06 07:40:08 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,10 +4,10 @@
 int is_palindrome_helper(char *s, int start, int end);
 
 /**
- * is_palindrome - function that checks for a palindrome
- * @s: string being checked
- *
- * Return: 1 if a palindrome, 0 if not
+ *is_palindrome - function that checks for a palindrome
+ *@s: string being checked
+ *is_palindrome_helper - function that does the actual check
+ *Return: 1 if a palindrome, 0 if not
  */
 int is_palindrome(char *s)
 {
@@ -16,13 +16,13 @@ int is_palindrome(char *s)
 
 int is_palindrome_helper(char *s, int start, int end)
 {
-	if (start >= end) 
+	if (start >= end)
 	{
 		return (1);
 	}
-	if (s[start] != s[end]) 
+	if (s[start] != s[end])
 	{
 		return (0);
 	}
 	return (is_palindrome_helper(s, start + 1, end - 1));
-}	
+}
