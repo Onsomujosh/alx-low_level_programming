5b6512b6ae316c52eeddeb89bc8f15f86104d15f
Onsomujosh <Joshuaonsomu@gmail.com>
2023-04-06 09:24:36 -0700

    identical strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,36 +8,27 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '*')
-	{
-		while (*s2 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-			{
-				return (1);
-			}
-			s1++;
-		}
-		return (wildcmp(s1, s2 + 1));
-	}
-	else if (*s2 == '\0')
+	if (*s2 == '\0')
 	{
 		return ((*s1 == '\0') ? 1 : 0);
 	}
-	else if (*s1 == '\0')
+	
+	if (*s2 == '*')
 	{
-		while (*s2 == '*')
+		if (wildcmp(s1, s2 + 1))
 		{
-			s2++;
+			return (1);
 		}
-		return ((*s2 == '\0') ? 1 : 0);
+
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+		{
+			return (1);
+		}
+		return (0);
 	}
-	else if (*s1 == *s2 || *s2 == '?')
+	if (*s1 != '\0' && (*s1 == *s2 || *s2 == '?'))
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
