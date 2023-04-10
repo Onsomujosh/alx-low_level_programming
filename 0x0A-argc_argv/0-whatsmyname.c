3d78cd1d1ee2a8c6e3892c9dea94ecaef092f2f7
Onsomujosh <Joshuaonsomu@gmail.com>
2023-04-10 12:17:11 -0700

    4th of his name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  *
  * Return: Always 0(Success)
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
