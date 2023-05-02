bb8fcbf2be1b55bac8593edb877087f2e6d667ad
Onsomujosh <Joshuaonsomu@gmail.com>
2023-05-02 14:37:05 -0700

    free safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,15 +9,32 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t count = 0;
+	int diff;
 	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
 
 	while (*h != NULL)
 	{
-		temp = *h;
-		*h = (*h)->next;
-		free(temp);
-		count++;
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			count++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			count++;
+			break;
+		}
 	}
+
+	*h = NULL;
 
 	return (count);
 }
