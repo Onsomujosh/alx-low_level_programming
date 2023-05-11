dabe02a93ac62137bfdb7462a0fcd4a4bd639b65
Onsomujosh <Joshuaonsomu@gmail.com>
2023-05-11 12:06:57 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (-10);
+		return (-1);
 	}
 	file = open(filename, O_APPEND | O_RDWR, 0664);
 	if (file == -1)
