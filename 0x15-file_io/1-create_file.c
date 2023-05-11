b1fd10a7f52e0173abd626489faad671c298f21f
Onsomujosh <Joshuaonsomu@gmail.com>
2023-05-11 11:44:10 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fp = fopen(filename, "w");
+	fp = fopen(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fp == NULL)
 		return (-1);
 
